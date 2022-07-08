33cfc54fdd41f54db82a58316edaf6679b9f043e
JaquelineRocio <jaquelineramosvargas@gmail.com>
2022-07-08 15:52:32 -0500

    save users in atlas

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,30 @@
+import { User } from "../models/User.js";
 
-
-export const register =  (req, res)=>{
+export const register =  async (req, res)=>{
   
-    console.log(req.body);
-    res.json({ok: 'register'})
+    const {email, password} = req.body;
+    try {
+        let user = await User.findOne({email})
+        if(user) throw {code: 11000};
+        
+        user = new User({email, password});
+        await user.save()
+
+        //jwt token
+
+        return res.status(201).json({ok: true})
+    } catch (error) {
+        console.log(error);
+        //Alternativa por mongoose
+        if(error.code === 11000)
+        {
+            return res.status(400).json({error: "Ya existe este usuario"})
+        }
+
+        
+    }
 };
 
-export const login = (req, res)=>{
+export const login = async (req, res)=>{
     res.json({ok: 'login'})
 };
