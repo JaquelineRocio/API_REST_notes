44f84fd07fc054428bc8473600ee6951f1a58a92
JaquelineRocio <jaquelineramosvargas@gmail.com>
2022-07-17 22:33:29 -0500

    implementing cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config'
 import './database/connectdb.js';
 import authRouter from './routes/auth.route.js';
@@ -6,6 +7,18 @@ import noteRouter from './routes/note.route.js';
 import cookieParser from 'cookie-parser';
 
 const app = express();
+
+const whiteList = [process.env.ORIGIN1]
+
+app.use(cors({
+    origin: function(origin, callback){
+        if(whiteList.includes(origin)){
+            return callback(null, origin)
+        }
+        return callback("Error de CORS origin:")
+    }
+}))
+
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/v1/auth', authRouter)
